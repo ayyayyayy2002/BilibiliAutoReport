ed2195e8879f932bb34ea2cde41c0f8bd7fa2dff
ayyayyayy2002 <joshualovejoy@outlook.com>
2024-12-07 16:54:44 +0800

    Update Capcha.py

--- Capcha.py
+++ Capcha.py
@@ -26,7 +26,7 @@ def capcha(aid):
     options.add_argument('--proxy-bypass-list=*')
     # options.add_argument("--disable-sync")
     # options.add_argument("disable-cache")#禁用缓存
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument('log-level=3')
     service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=options)  # 启动 Chrome 浏览器
@@ -48,9 +48,11 @@ def capcha(aid):
         top_y = int(rect['top'])
         return left_x, top_y
     while True:
-        url = f"https://www.bilibili.com/appeal/?avid={aid}"
-        driver.get(url)
+
         try:
+            url = f"https://www.bilibili.com/appeal/?avid={aid}"
+            driver.get(url)
+
             WebDriverWait(driver, 20, 1).until(
                 EC.presence_of_element_located(
                     (By.XPATH, '/html/body/div/div/div[2]/div[1]/div[2]/div[1]/div'))
@@ -74,11 +76,9 @@ def capcha(aid):
                 except Exception:
                     print("验证码元素未出现，重新点击确认...")
                 time.sleep(10)
-        except Exception as e:
-            print(f"发生错误: {e}")
 
-        while True:
-            try:
+
+            while True:
                 img = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located(
                         (By.XPATH, '//*[@class="geetest_item_wrap"]'))
@@ -113,53 +113,47 @@ def capcha(aid):
                                                         -(b + y * lan_y)).perform()  # 恢复鼠标位置
                     time.sleep(0.3)
 
-                try:  # 执行点击确认按钮的操作
-                    element = WebDriverWait(driver, 10).until(
-                        EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_commit_tip'))
+                # 执行点击确认按钮的操作
+                element = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_commit_tip'))
+                )
+                element.click()  # 提交验证码
+
+
+                '''
+                try:
+                    refresh_element = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_refresh'))
                     )
-                    element.click()  # 提交验证码
+                    refresh_element.click()  # 点击刷新验证按钮
+                    print('已点击刷新按钮')
                 except Exception as e:
-                    print(f"提交验证码时发生错误: {e}")
-                    try:
-                        refresh_element = WebDriverWait(driver, 10).until(
-                            EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_refresh'))
-                        )
-                        refresh_element.click()  # 点击刷新验证按钮
-                        print('已点击刷新按钮')
-                    except Exception as e:
-                        print('点击刷新按钮出错！')
+                    print('点击刷新按钮出错！')
+                '''
 
-                try:  # 等待 'geetest_item_wrap' 元素消失，表示验证码提交成功
-                    WebDriverWait(driver, 3).until(
-                        EC.invisibility_of_element_located(
-                            (By.XPATH, '//*[@class="geetest_item_wrap"]'))
-                    )
-                    print("验证码已消失！")
+                WebDriverWait(driver, 3).until(EC.invisibility_of_element_located((By.XPATH, '//*[@class="geetest_item_wrap"]')))
+                print("验证码已消失！")# 等待 'geetest_item_wrap' 元素消失，表示验证码提交成功
+                WebDriverWait(driver, 5).until(EC.alert_is_present())  # 等待最多5秒，直到弹窗出现
+                alert = driver.switch_to.alert  # 切换到弹窗
+                alert.accept()
 
-                    try:
-                        WebDriverWait(driver, 5).until(EC.alert_is_present())  # 等待最多5秒，直到弹窗出现
-                        alert = driver.switch_to.alert  # 切换到弹窗
-                        alert_text = alert.text  # 获取弹窗文本
-                        # 检查弹窗内容
-                        if not "-352" in alert_text:
-                            alert.accept()
-                            print("验证码验证成功！")
-                            break
-                        else:
-                            print('意外情况，弹窗出现-352')
 
-                    except TimeoutException:
-                        print('多次验证失败，程序退出')
+                break
+            break
+        except Exception as e:
+            print(f'人机验证出错{e}')
 
 
 
-                except Exception as e:
-                    print(f"验证码验证失败！，错误: {e}")
 
 
-            except Exception as e:
-                print(f"人机验证循环出错，错误: {e}")
-        cookies = driver.get_cookies()
-        COOKIE = '; '.join([f"{cookie['name']}={cookie['value']}" for cookie in cookies])
-        driver.quit()
-        return COOKIE
+
+
+
+
+
+
+    cookies = driver.get_cookies()
+    COOKIE = '; '.join([f"{cookie['name']}={cookie['value']}" for cookie in cookies])
+    driver.quit()
+    return COOKIE
