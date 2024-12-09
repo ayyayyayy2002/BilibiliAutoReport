270b60a12243edc1b1a0495cb9123771641e51d4
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-12-09 11:40:04 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -14,28 +14,26 @@ skip = True
 
 
 while True:
-    while True:
-        if skip:
-            skip = False
-            print('首次次运行跳过')
-            break
+    if not skip:
+        while True:
+            print('启动Getuid.py')
+            getuid_process = subprocess.Popen([sys.executable, 'GetUid.py'], shell=True)
+            getuid_process.wait()  # 等待 Getuid.py 结束
 
+
+            if getuid_process.returncode == 0:# 检查 Getuid.py 的退出状态
+                print("Getuid.py 正常退出，正在启动 Report.py...")
+                break  # 退出此循环，开始启动 Report.py
+            else:
+                error_message = f"Getuid.py 出现错误，返回码: {getuid_process.returncode}，正在重新运行 Getuid.py..."
+                print(error_message)
         print('启动SpaceAndDynamic.py')
         SpaceAndDynamic_process = subprocess.Popen([sys.executable, 'SpaceAndDynamic.py'], shell=True)
 
-        print('启动Getuid.py')
-        getuid_process = subprocess.Popen([sys.executable, 'GetUid.py'], shell=True)
-        getuid_process.wait()  # 等待 Getuid.py 结束
 
-
-        if getuid_process.returncode == 0:# 检查 Getuid.py 的退出状态
-            print("Getuid.py 正常退出，正在启动 Report.py...")
-            break  # 退出此循环，开始启动 Report.py
-        else:
-            error_message = f"Getuid.py 出现错误，返回码: {getuid_process.returncode}，正在重新运行 Getuid.py..."
-            print(error_message)
-
-
+    else:
+        print('首次运行跳过')
+        skip = False
     while True:
         #report_process.wait()  # 不等待 SpaceAndDynamic.py 结束
         print('启动Report.py')
