da608936afea6fc677194a2f88e7a4c86541566a
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-12-11 15:13:08 +0800

    Update Capcha.py

--- Capcha.py
+++ Capcha.py
@@ -16,21 +16,18 @@ def capcha(aid,i):
     path_name = f'User Data{i}'
     proxies = {'http': None, 'https': None}
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    chrome_driver_path = os.path.join(base_dir, '附加文件', 'chromedriver.exe')
-    chrome_binary_path = os.path.join(base_dir, '附加文件', 'chrome-win', 'chrome.exe')
     user_data_dir = os.path.join(base_dir, '附加文件', 'User Data',path_name)
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")
     options.add_argument(f'--user-data-dir={user_data_dir}')  # 设置用户数据目录
-    options.binary_location = chrome_binary_path  # 指定 Chrome 浏览器的可执行文件路径
     options.add_argument('--proxy-server="direct://"')
     options.add_argument('--proxy-bypass-list=*')
     # options.add_argument("--disable-sync")
     # options.add_argument("disable-cache")#禁用缓存
     options.add_argument("--headless")
     options.add_argument('log-level=3')
-    service = Service(executable_path=chrome_driver_path)
-    driver = webdriver.Chrome(service=service, options=options)  # 启动 Chrome 浏览器
+    driver = webdriver.Chrome(options=options)  # 启动 Chrome 浏览器
     driver.set_window_size(1000, 700)  # 设置浏览器窗口大小（宽度, 高度）
 
     def get_location(target):
