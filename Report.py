7b6f8f776ff255b39c838d43d83c01ef5efbca0a
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-12-17 14:43:59 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -26,16 +26,16 @@ tids_with_weights = {
     '2':1,  # 违法违禁
     '5':1,  # 赌博诈骗
     '10025':1,  # 违法信息外链
-    '10014':1,#涉政谣言
-    '10015':1,#涉社会事件谣言
+    '10014':3,#涉政谣言
+    '10015':4,#涉社会事件谣言
     '10017':1,#虚假不实信息
-    '10018':1,#违规推广
-    '52':1,#转载/自制错误
-    '10019':1,#其他不规范行为
+    '10018':4,#违规推广
+    '52':3,#转载/自制错误
+    '10019':2,#其他不规范行为
     '7':1,#人身攻击
     '9':1,#引战
     '3':1,#色情低俗
-    '10020':1,#危险行为
+    '10020':2,#危险行为
     '10021':1,#观感不适
     '6':1,#血腥暴力
     '10000':1,#青少年不良信息
@@ -89,16 +89,25 @@ for uid in uids:
     aids = []
     titles = []
     pics = []
-    try:
-        with open(uid_file, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-        with open(uid_file, 'w', encoding='utf-8') as f:
-            for line in lines:
-                if line.strip() != uid:
-                    f.write(line)
-        print(f"删除UID: {uid}")
-    except Exception as e:
-        print(f"删除UID时发生错误: {e}")
+
+    headers = {'cookie': COOKIE1, 'user-agent': UA}
+    search_url = f'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid={uid}&type=video'
+    response = requests.get(search_url, headers=headers, proxies=proxies, timeout=(5, 10))
+    data = response.json()
+    for item in data["data"]["items"]:
+        # 检查当前 item 的 "text" 字段是否为 "动态视频"
+        text = item["modules"]["module_dynamic"]["major"]["archive"]["badge"]["text"]
+        if text == "动态视频":
+            # 提取 aid, title, cover 并添加到对应的集合
+            aids.append(item["modules"]["module_dynamic"]["major"]["archive"]["aid"])
+            titles.append(item["modules"]["module_dynamic"]["major"]["archive"]["title"])
+            pics.append(item["modules"]["module_dynamic"]["major"]["archive"]["cover"])
+
+    print(f'动态视频个数:{len(aids)},动态标题个数:{len(titles)},动态封面个数:{len(pics)}')
+
+
+
+
 
     headers = {'cookie': COOKIE1, 'user-agent': UA}
     search_url = f'https://api.bilibili.com/x/polymer/space/seasons_series_list?mid={uid}&page_num=1&page_size=5'
@@ -133,7 +142,7 @@ for uid in uids:
         titles.append(archive['title'])
         pics.append(archive['pic'])
 
-    print(f'全部视频个数:{len(aids)},全部标题个数:{len(titles)},普全部封面个数:{len(pics)}')
+    print(f'全部视频个数:{len(aids)},全部标题个数:{len(titles)},全部封面个数:{len(pics)}')
     aid_log_file = os.path.join(base_dir, '运行记录', 'UID记录', f'{uid}.txt')
     with open(aid_log_file, 'a', encoding='utf-8') as file:
         file.write(f"普通视频个数:{len(aids)},普通标题个数:{len(titles)},普通图片个数:{len(pics)}\n")
@@ -156,13 +165,15 @@ for uid in uids:
                 'attach': pic,
                 'block_author': 'false',
                 'csrf': CSRF,
-                'desc': f'违规行为：在视频标题及评论中支持“台独”行为，并辱骂讽刺政府和领导人。视频标题{title}违规推广色情游戏，视频封面是色情游戏截图。在置顶动态和评论发布加密链接传播色情内容，严重违反刑法第三百六十四条【传播淫秽物品罪】 传播淫秽的书刊、影片、音像、图片或者其他淫秽物品。诉求：下架此视频并处罚发送此视频的账号',
+                'desc': f'违规行为：在视频标题{title}及评论中支持“台独”行为，并辱骂讽刺政府和领导人。诉求：下架此视频并处罚发送此视频的账号',
+
+                #'desc': f'违规行为：在视频标题及评论中支持“台独”行为，并辱骂讽刺政府和领导人。视频标题{title}违规推广色情游戏，视频封面是色情游戏截图。在置顶动态和评论发布加密链接传播色情内容，严重违反刑法第三百六十四条【传播淫秽物品罪】 传播淫秽的书刊、影片、音像、图片或者其他淫秽物品。诉求：下架此视频并处罚发送此视频的账号',
                 'tid': tid
             }
             response = requests.post('https://api.bilibili.com/x/web-interface/appeal/v2/submit', headers=headers,
                                      data=data, proxies=proxies, timeout=(3, 3))
             print(f'账号{i}视频{reportcount:03}:{response.text}')
-            if "62009" in response.text or reportcount >= 200:
+            if "62009" in response.text or reportcount >= 65:
                 break
             elif "-352" in response.text:
                 COOKIE = capcha(aid, i)
@@ -184,6 +195,19 @@ for uid in uids:
             aid_log_file = os.path.join(base_dir, '运行记录', 'UID记录', f'{uid}.txt')
             with open(aid_log_file, 'a', encoding='utf-8') as file:
                 file.write(f'{aid},{tid}\n')
+
+
+    try:
+        with open(uid_file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        with open(uid_file, 'w', encoding='utf-8') as f:
+            for line in lines:
+                if line.strip() != uid:
+                    f.write(line)
+        print(f"删除UID: {uid}")
+    except Exception as e:
+        print(f"删除UID时发生错误: {e}")
+
 
 with open(log_file, 'a', encoding='utf-8') as log:
     timestamp = datetime.now().strftime('[%Y-%m-%d %H-%M-%S]')
